8541382fd01de8152dc9acc1bce75fe8c6e6dc7b
DerianW <ScratchGobo@gmail.com>
2018-09-12 14:28:01 -0400

    bfgstb

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+public class main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("*** Output ***") ;
+
+        int count = 1 ;
+        while (count <= 10)
+        {
+            System.out.println(count * count);
+            count++ ;
+        }
+
+    }
+}
